8544055044b503d93eb61da8ee6e479de3e96362
Nick Tomlin <nick.tomlin@gmail.com>
2015-04-16 17:57:12 -0500

    Add comment store

---
+++ client/js/stores/comment-store.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var store = require('../lib/base-store');
+var events = require('../constants').events;
+var log = require('../lib/log')('stores:comment-store');
+
+module.exports = function (TilStore) {
+  var _comments = {};
+
+  function add (comment) {
+    if (_comments[comment.tilId]) {
+      _comments[comment.tilId].push(comment);
+    } else {
+      _comments[comment.tilId] = [comment];
+    }
+    log('updated', _comments);
+  }
+
+  return store({
+    get: function () {
+      return _comments;
+    },
+    getForTil: function (tilId) {
+      return _comments[tilId] || [];
+    },
+    handler: function (type, payload) {
+      switch(type) {
+        case events.ADD_COMMENT:
+          log('add', payload);
+          this.waitFor(TilStore.dispatchToken);
+          add(payload.comment);
+          this.emitChange();
+        break;
+      }
+    }
+  });
+};
