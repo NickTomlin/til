ebfe19ffd4c5b36d4bad73689b201ff2a48bb495
Nick Tomlin <nick.tomlin@gmail.com>
2015-04-16 18:00:11 -0500

    Update client and server actions

--- client/js/actions/client-actions.js
+++ client/js/actions/client-actions.js
@@ -1,15 +1,25 @@
 'use strict';
 
 var dispatcher = require('../dispatcher');
+var uuid = require('node-uuid').v1;
 var log = require('../lib/log')('actions:client');
 var events = require('../constants').events;
 
 module.exports = function () {
   this.addTIL = function (til) {
-    log('addTil', til);
+    log(events.ADD_TIL, til);
+    til.id = uuid();
     dispatcher.dispatch({
       type: events.ADD_TIL,
       til: til
+    });
+  },
+  this.addComment = function (comment) {
+    log(events.ADD_COMMENT, comment);
+    comment.id = uuid();
+    dispatcher.dispatch({
+      type: events.ADD_COMMENT,
+      comment: comment
     });
   }
 };
--- client/js/actions/server-actions.js
+++ client/js/actions/server-actions.js
@@ -1,19 +1,21 @@
 'use strict';
 
 var dispatcher = require('../dispatcher');
+var uuid = require('node-uuid').v1;
 var log = require('../lib/log')('actions:server');
 var events = require('../constants').events;
 
 module.exports = function () {
   this.addUser = function (user) {
-    log('addUser', user);
+    log(events.ADD_USER, user);
     dispatcher.dispatch({
       type: events.ADD_USER,
       user: user
     });
   },
   this.receiveTil = function (til) {
-    log('addTil', til);
+    log(events.RECEIVE_TIL, til);
+    til.id = uuid();
     dispatcher.dispatch({
       type: events.RECEIVE_TIL,
       til: til
