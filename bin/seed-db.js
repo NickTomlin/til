579ab04a4bafc8a81932b4a7e24b3c385ac062ed
Nick Tomlin <nick.tomlin@gmail.com>
2015-04-22 17:56:58 -0500

    Update seed script to actually work

--- bin/seed-db.js
+++ bin/seed-db.js
@@ -7,36 +7,58 @@ var models = require('../server/models');
 var data = {
   til: [
     {
-      text: 'you can toggle between #vim splits by using ctrl + w + | and ctrl + w + ='
+      text: 'you can toggle between #vim splits by using ctrl + w + | and ctrl + w + =',
+      userId: '0'
     },
     {
-      text: '#angular\'s ng-repeat track-by attribute is a wonderful way to achieve performant DOM updates'
+      text: '#angular\'s ng-repeat track-by attribute is a wonderful way to achieve performant DOM updates',
+      userId: '0'
     },
     {
-      text: '#mongoose has native es6 support built into version 3.9.X'
+      text: '#mongoose has native es6 support built into version 3.9.X',
+      userId: '0'
+    }
+  ],
+  comment: [
+    {
+      text: 'hey this is a great comment'
+    },
+    {
+      text: 'I disagree'
     }
   ]
 };
 
-function seed (cb) {
-  Object.keys(data).forEach(function (model, dataIndex, dataKeys) {
-    var Model = models[model];
-    Model.create(data[model], function (err) {
-      if (err) { throw new Error(err); }
-      if (dataIndex + 1 === dataKeys.length) {
-        cb();
-      }
-    });
+function done () {
+  mongoose.disconnect(function () {
+    console.log('Done seeding database.');
   });
 }
 
-mongoose.connect(config.get('db'), function () {
-  mongoose.connection.db.dropDatabase(function () {
-    console.log('Dropped Database. Seeding');
-    seed(function () {
-      mongoose.disconnect(function () {
-        console.log('Done seeding database.');
+function seed (modelName, cb) {
+  console.log('Seeding for ', modelName);
+  data[modelName].reduce(function (results, modelData, dataIndex, dataKeys) {
+    models[modelName].create(modelData, function (err, result) {
+      if (err) { throw new Error(err); }
+      results.push(result);
+      if (dataIndex + 1 === dataKeys.length) {
+        cb(results);
+      }
+    });
+    return results;
+  }, []);
+}
+
+try {
+  mongoose.connect(config.get('db'), function () {
+    mongoose.connection.db.dropDatabase(function () {
+      console.log('Dropped Database. Seeding');
+      seed('til', function () {
+        seed('comment', done);
       });
     });
   });
-});
+} catch (e) {
+  console.log('Err', e);
+  done();
+}
